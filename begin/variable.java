b6fdb0fd5b1c5537aadf0e5bf4ce1fb585e561ae
Mr Lii <leetamparega@gmail.com>
2025-02-21 21:51:58 +0000

    start

---
+++ begin/variable.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // Define the userXP variable
+        int userXP = 123456;
+        // Define the userHealth variable
+        double userHealth = 99.9;
+        boolean isSmoker = false;
+        
+        System.out.println(userXP);
+        System.out.println(userHealth);
+        // Print out the value of isSmoker
+        System.out.println(isSmoker);
+    }
+}
