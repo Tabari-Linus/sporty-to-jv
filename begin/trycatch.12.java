6e417ffcf94c0cc69394241ef7812df44052821e
Mr Lii <leetamparega@gmail.com>
2025-02-22 06:00:32 +0000

    try_catch

---
+++ begin/trycatch.12.java
@@ -0,0 +1,28 @@
+class BetterThanAverage {
+    public static void main(String[] args) {
+        int[] testScores = {89, 82, 64, 97, 80, 89, 70, 99};
+        String[] students = {"Sally", "Muhammad", "Alex", "Ramesh", "George", "Kate", "Kelly", "Phoenix"};
+        int average =  computeAverage(testScores);
+        // Call on determineBest with the students, testScores, and average
+        determineBest(students, testScores, average);
+    }
+    
+    public static int computeAverage(int[] scores){
+        int total = 0; 
+        // Loop through each of the scores
+        for (int score: scores){
+            total += score;
+        }
+        // return the average score
+        return total/scores.length;
+    }
+    
+    public static void determineBest(String[] names, int[] scores, int avg){
+        for (int i = 0; i < names.length; i++) {
+            // Add a condition to see if a student's score is better than average
+            if (scores[i] > avg) {
+                System.out.println (names[i] + " scored better than average");
+            }
+        }
+    }
+}
