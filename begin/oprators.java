6e45ceba3098f16275be0e3f914baddba81c78af
Mr Lii <leetamparega@gmail.com>
2025-02-22 00:38:08 +0000

    operators

---
+++ begin/oprators.java
@@ -0,0 +1,20 @@
+class OperatorsAtWork {
+   
+    public static void main(String[] args) {
+        int scoreTest1 = 78;
+        int scoreTest2 = 94;
+        // Assign testTotal the sum of scores
+        int testTotal = scoreTest1 + scoreTest2;
+        // Assign avg the average
+        int avg = testTotal / 2;
+        int passingScore = 65;
+        // Incrementor passingScore
+        passingScore ++;
+        // Assign the isPassing variable 
+        boolean isPassing = avg >= passingScore;
+        System.out.println(testTotal);
+        System.out.println(avg);
+        System.out.println(passingScore);
+        System.out.println(isPassing);
+    }
+}
