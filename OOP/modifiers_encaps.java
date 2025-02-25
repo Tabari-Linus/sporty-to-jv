bf9ce3e84c0c6c39a276c64599e40a8519cee597
Mr Lii <leetamparega@gmail.com>
2025-02-25 05:58:34 +0000

    encapsulation

---
+++ OOP/modifiers_encaps.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static class Book {
+        private int pages;
+        private String author;
+        private String title;
+        private boolean isRented;
+        
+        // Set the access modifier for the constructor
+        public Book(String title, String author, int pages) {
+        	this.pages = pages;
+            this.author = author;
+            this.title = title;
+            this.isRented = false;
+        }
+        
+        // Set the access modifier for the methods
+        public void printDetails() {
+        	System.out.println(prepareDetails());
+        }
+        
+        private String prepareDetails() {
+            return "Book{title='" + title + "', isRented=" + isRented + ", author='" + author + "', pages=" + pages +  "}";
+        }
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Moby-Dick", "Herman Melville", 635);
+        book.printDetails();
+    }
+}
